f64c2af198b2615ce15bd65c3ab283475a6280ca
Giovanne Mariano <giolucasd@gmail.com>
2025-11-22 13:57:01 -0300

    refactor: update weights downloader

--- download_weights.sh
+++ download_weights.sh
@@ -1,4 +1,7 @@
-wget https://github.com/giolucasd/ssrl-vit-mae-jepa/releases/download/v1.0/mae_weights_v1.zip
+wget https://github.com/giolucasd/ssrl-vit-mae-jepa/releases/download/v1.0/mae_classifier_weights_v1.zip
+wget https://github.com/giolucasd/ssrl-vit-mae-jepa/releases/download/v1.0/vit-mae.pt
+mkdir -p assets/weights/
+mv vit-mae.pt assets/weights/
 unzip mae_weights_v1.zip -d assets/weights/
 mv assets/weights/mae_weights_v1/* assets/weights/
 rm -r assets/weights/mae_weights_v1/
